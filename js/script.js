63c43f7ee4127e0acd406338c812592a472aabfd
Allen <AALLEN6@travelers.com>
2021-01-27 15:54:13 -0500

    meets expectations
    full meets except for comments

--- js/script.js
+++ js/script.js
@@ -61,14 +61,13 @@ design.addEventListener('change', (e) => {
 
 registerActivity.addEventListener('change', (e) => {
   const dataCost = +(e.target.getAttribute('data-cost'))
-  // need to disable conflicting times for checkboxes
+  // need to disable conflicting times for checkboxes for exceeds
   if (e.target.checked === true) {
     totalCost += dataCost
   } else {
     totalCost -= dataCost
   }
   activityCost.innerHTML = `Total: $${totalCost}`
-  console.log(totalCost)
 })
 
 payMethod.addEventListener('change', (e) => {
@@ -93,31 +92,87 @@ form.addEventListener('submit', (e) => {
   const cardNum = cardNumInput.value
   const zip = zipInput.value
   const cvv = cvvInput.value
-  const nameTest = /^[a-z]+$/.test(name)
+  const nameTest = /^[a-z]+$/i.test(name)
   const emailTest = /^[^@]+@[^@.]+\.[a-z]+$/i.test(email)
   const cardNumTest = /^\d{13,16}$/.test(cardNum)
   const zipTest = /^\d{5}$/.test(zip)
   const cvvTest = /^\d{3}$/.test(cvv)
 
-  if (!nameTest || !emailTest || !cardNumTest || !zipTest || !cvvTest) {
+  if (!nameTest || !emailTest) {
     e.preventDefault()
+
+    if (!nameTest) {
+      document.querySelector('#name-hint').style.display = 'block'
+    } else {
+      document.querySelector('#name-hint').style.display = 'none'
+    }
+
+    if (!emailTest) {
+      document.querySelector('#email-hint').style.display = 'block'
+    } else {
+      document.querySelector('#email-hint').style.display = 'none'
+    }
+  } else {
+    document.querySelector('#name-hint').style.display = 'none'
+    document.querySelector('#email-hint').style.display = 'none'
   }
 
-  for (let i = 0; i < checkboxes.length; i++) {
-    if (!checkboxes[0].checked &&
-      !checkboxes[1].checked &&
-      !checkboxes[2].checked &&
-      !checkboxes[3].checked &&
-      !checkboxes[4].checked &&
-      !checkboxes[5].checked &&
-      !checkboxes[6].checked) {
+  if (document.querySelector('option[value="credit-card"]').selected) {
+    if (!cardNumTest || !zipTest || !cvvTest) {
       e.preventDefault()
+
+      if (!cardNumTest) {
+        document.querySelector('#cc-hint').style.display = 'block'
+      } else {
+        document.querySelector('#cc-hint').style.display = 'none'
+      }
+
+      if (!zipTest) {
+        document.querySelector('#zip-hint').style.display = 'block'
+      } else {
+        document.querySelector('#zip-hint').style.display = 'none'
+      }
+
+      if (!cvvTest) {
+        document.querySelector('#cvv-hint').style.display = 'block'
+      } else {
+        document.querySelector('#cvv-hint').style.display = 'none'
+      }
+    } else {
+      document.querySelector('#cc-hint').style.display = 'none'
+      document.querySelector('#zip-hint').style.display = 'none'
+      document.querySelector('#cvv-hint').style.display = 'none'
     }
   }
 
-  console.log(nameTest)
-  console.log(emailTest)
-  console.log(cardNumTest)
-  console.log(zipTest)
-  console.log(cvvTest)
+  if (!checkboxes[0].checked &&
+    !checkboxes[1].checked &&
+    !checkboxes[2].checked &&
+    !checkboxes[3].checked &&
+    !checkboxes[4].checked &&
+    !checkboxes[5].checked &&
+    !checkboxes[6].checked) {
+    e.preventDefault()
+    document.querySelector('#activities-hint').style.display = 'block'
+  } else {
+    document.querySelector('#activities-hint').style.display = 'none'
+  }
 })
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener('focus', (e) => {
+    if (!e.target.checked) {
+      checkboxes[i].parentNode.className = 'focus'
+    } else {
+      e.target.parentNode.className = ''
+    }
+  })
+
+  checkboxes[i].addEventListener('blur', (e) => {
+    if (e.target.checked) {
+      checkboxes[i].parentNode.className = 'focus'
+    } else {
+      e.target.parentNode.className = ''
+    }
+  })
+}
